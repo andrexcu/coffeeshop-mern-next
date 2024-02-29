bd8f479a608ebce350a33b5b26d231518cc519fa
Andrex Cullo <andrexcu@gmail.com>
2024-02-29 21:34:25 +0800

    style: hero section fix

--- client/components/Sections/Contact.tsx
+++ client/components/Sections/Contact.tsx
@@ -9,10 +9,10 @@ const Contact = () => {
   return (
     <div className="max-w-7xl w-full mt-12">
       <SectionTitle title="Contact" subtitle="Contact Us" />
-      <div className="relative flex justify-center items-center ">
-        <div className="flex flex-col lg:flex-row justify-center items-start mx-auto max-w-7xl w-full lg:h-[600px] z-10">
+      <div className="relative flex justify-center items-center">
+        <div className="flex flex-col lg:flex-row justify-center items-start mx-auto max-w-7xl w-full z-10">
           {/* <div className="flex flex-col lg:flex-row z-10 border"> */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:flex flex-col items-start justify-start gap-y-12 p-4 w-full lg:w-1/3 h-full text-slate-200 ">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:flex flex-col items-start justify-start gap-y-12 p-4 w-full lg:w-1/3 text-slate-200 ">
             <div className="flex gap-x-4">
               <div className="flex justify-center items-center bg-orange-300 rounded-full h-12 w-12">
                 <MapPin className="h-6 w-6" />
@@ -59,9 +59,9 @@ const Contact = () => {
               </div>
             </div>
           </div>
-          <form
+          <div
             onSubmit={handleSubmit}
-            className="grid grid-cols-2 gap-x-4 items-start justify-start p-4 w-full gap-y-8 lg:w-2/3  text-slate-200  border-gray-500"
+            className="sm:grid grid-cols-2 gap-x-4 sm:items-start sm:justify-start  items-center justify-center p-4 gap-6 space-y-6 sm:space-y-0 w-full text-slate-200  border-gray-500"
           >
             <input
               type="text"
@@ -87,7 +87,7 @@ const Contact = () => {
             <button className="text-white w-48 py-2 col-span-2 rounded-full bg-orange-300 transition duration-300 ease-in hover:bg-stone-800">
               Send Message
             </button>
-          </form>
+          </div>
         </div>
       </div>
     </div>
--- client/components/Sections/Hero.tsx
+++ client/components/Sections/Hero.tsx
@@ -17,21 +17,25 @@ const Hero = ({ scrollToContact }: HeroProps) => {
   // }, []);
 
   return (
-    <div className="min-h-dvh relative flex bg-black">
+    <div className="min-h-dvh bg-black">
       <div
-        className="flex flex-col items-center justify-center w-full min-h-screen z-0 bg-cover bg-fixed"
+        className="flex flex-col-reverse lg:flex-row items-center lg:items-end justify-center w-full min-h-screen z-0 bg-cover bg-fixed "
         style={{
           backgroundImage: 'url("/images/cover-right3.jpg")',
           backgroundPosition: "center",
         }}
       >
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
-
-        <div className="z-20 lg:absolute right-12 ml-0 lg:ml-52 pr-4 flex flex-col lg:flex-row items-center justify-start my-10 mt-40 min-h-[500px] hero-framed lg:p-72 w-[60%] pt-8">
+        <div className="w-full flex items-center justify-center mb-[60px] lg:mb-[200px]">
+          <Link href="https://www.youtube.com/watch?v=nyDnQQSUfL0" className="">
+            <div className="play-btn outline-none"></div>
+          </Link>
+        </div>
+        <div className="mt-32 gap-12 xl:min-w-[720px] z-20 p-4 flex flex-col lg:flex-row items-center justify-end hero-framed lg:mr-[60px] mb-[100px] pt-8">
           {/* <div className="border mt-14 w-[400px] h-[200px]">asdas</div> */}
-          <div className="lg:absolute lg:right-[400px] lg:mt-36 ml-0 lg:ml-16 flex gap-8 flex-col justify-center items-center w-[500px] h-[300px] p-4 ">
+          <div className="space-y-4  flex-col justify-center items-center ">
             <div
-              className={`flex flex-col justify-center h-[175px] w-[325px] bg-zinc-950/90 text-center p-2 ${urbanist.className}`}
+              className={`flex flex-col justify-center py-6 px-6 bg-zinc-950/90 text-center ${urbanist.className}`}
             >
               <p className="text-slate-200 text-2xl">Open Every Day</p>
               <p className="text-orange-300 text-2xl font-thin">
@@ -44,31 +48,30 @@ const Hero = ({ scrollToContact }: HeroProps) => {
                 Sunday: 07:00 - 24:00
               </p>
             </div>
-            <div className="flex gap-x-8">
+            <div className="flex flex-col sm:flex-row justify-between items-center gap-4">
               {/* bg-[#3D2B1F]/90 */}
-              <div className="border-2 border-orange-300 bg-zinc-950/50 text-slate-200 p-4 cursor-pointer min-w-[150px] text-center transition duration-300 ease-in hover:bg-[#3D2B1F]">
+              <div className="border-2 border-orange-300 bg-zinc-950/50 text-slate-200 p-4 cursor-pointer  text-center transition duration-300 ease-in hover:bg-[#3D2B1F]">
                 ORDER NOW
               </div>
               <div
                 onClick={scrollToContact}
-                className="border-2 border-orange-300 bg-zinc-950/50 text-slate-200 p-4 cursor-pointer min-w-[150px] text-center transition duration-300 ease-in hover:bg-[#3D2B1F]"
+                className="border-2 border-orange-300 bg-zinc-950/50 text-slate-200 p-4 cursor-pointer text-center transition duration-300 ease-in hover:bg-[#3D2B1F]"
               >
                 CONTACT US
               </div>
             </div>
           </div>
-          <div className="lg:absolute right-4">
+          <div className="right-4 ">
             <div
-              className={`s lg:mt-40 flex flex-col items-center sm:items-start max-w-[400px] h-[120px] sm:h-[175px] sm:border-l-4 border-[#1B1B1B] text-slate-200 ${lobsterTwo.className} p-4 `}
+              className={`flex flex-col items-center sm:items-start sm:border-l-4 border-[#1B1B1B] text-slate-200 ${lobsterTwo.className} p-4 `}
             >
               <p className="text-5xl sm:text-7xl">Premium.</p>
               <p className="flex gap-x-4 text-5xl sm:text-7xl">
                 <span className="text-orange-300">Coffee</span>
                 <span className="">Shop</span>
               </p>
-              {/* <p className="text-7xl flex sm:hidden">Shop</p> */}
             </div>
-            <div className="pl-8 text-slate-100 text-2xl sm:text-3xl flex flex-col gap-y-2 justify-center items-center sm:items-start gap-x-1 w-[375px] h-[100px] my-0 text-wrap">
+            <div className="pl-8 text-slate-100 text-2xl sm:text-3xl flex flex-col gap-y-2 justify-center items-center sm:items-start gap-x-1 my-0 text-wrap">
               <p>
                 Premium <span className="bg-[#1B1B1B]">Coffee,</span>
               </p>
@@ -78,12 +81,6 @@ const Hero = ({ scrollToContact }: HeroProps) => {
             </div>
           </div>
         </div>
-        <Link
-          href="https://www.youtube.com/watch?v=nyDnQQSUfL0"
-          className="pr-8 pb-8 lg:pr-0 pl-4 flex sticky lg:justify-end items-start lg:items-end lg:right-[1000px] 2xl:right-[1325px] lg:mt-48 2xl:mt-96"
-        >
-          <div className="play-btn outline-none"></div>
-        </Link>
       </div>
       {/* <div className="absolute sm:left-2.5 w-full bg-transparent min-h-[935px] page-framed z-1"></div> */}
     </div>
