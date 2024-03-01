0c50e1fdcb164bcdd9cc8328413e2c7c52f27190
Andrex Cullo <andrexcu@gmail.com>
2024-03-01 20:46:33 +0800

    feat: menu section btn

--- client/app/(routes)/order/components/MenuItem.tsx
+++ client/app/(routes)/order/components/MenuItem.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import "./menuItem.css";
+import { Button } from "@/components/ui/button";
 
 interface MenuItemProps {
   product: ProductType;
@@ -11,13 +12,27 @@ interface MenuItemProps {
 const MenuItem = ({ product }: MenuItemProps) => {
   return (
     <div className="grid-cols-12 menu ">
-      <div className="col-span-6 menu-item">
-        <img src={product.image} className="menu-img" alt="" />
+      <div className="relative col-span-6 menu-item group">
+        {/* <img src={product.image} className="menu-img" alt="" /> */}
+        <Image
+          src={product.image}
+          width={60}
+          height={50}
+          style={{ width: "68px", height: "65px" }}
+          alt="product image"
+          className="rounded-full float-left border-5 border-solid border-white border-opacity-20"
+        />
         <div className="menu-content">
-          <a>{product.name}</a>
+          <a className="z-0">{product.name}</a>
           <span>${product.price}</span>
         </div>
         <div className="menu-description">{product.description}</div>
+
+        <div className="z-20 absolute w-full h-full top-0 opacity-0 group-hover:opacity-100 flex justify-center items-center transition duration-300 ease-in">
+          <Button variant="orange" className="">
+            Add To Cart
+          </Button>
+        </div>
       </div>
     </div>
   );
