386a30695b2b0a5f18736fc2040f4c420cf529cf
andrexcu <andrexcu@gmail.com>
2024-06-08 12:12:18 +0800

    feat: vercel config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import app from "./app";
 
-const PORT = process.env.ENV || 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.DATABASE_URL!).then(() => {
   console.log(`listening on port ${PORT}`);
