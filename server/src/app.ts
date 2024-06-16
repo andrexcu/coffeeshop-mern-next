bd6efbb14be1b7bcfa78d0eac2ad37b32b089c76
andrexcu <andrexcu@gmail.com>
2024-06-16 18:40:39 +0800

    cookie update

--- server/src/app.ts
+++ server/src/app.ts
@@ -35,8 +35,10 @@ app.use(
     resave: false,
 
     cookie: {
-      maxAge: 60000 * 60,
+      httpOnly: true,
+      secure: true,
       sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     },
   })
 );
