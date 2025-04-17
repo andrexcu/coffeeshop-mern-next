01bde7060725ae979ddd1492073a50b7c5a44e17
andrexcu <andrexcu@gmail.com>
2025-04-17 20:07:58 +0800

    package update

--- server/src/jwt/token.ts
+++ server/src/jwt/token.ts
@@ -1,12 +1,11 @@
 import jwt from "jsonwebtoken";
-import { userType } from "../types/userType";
 
 export const generateAccessToken = (user: any) => {
 
   if (!process.env.ACCESS_TOKEN_SECRET) {
     throw new Error("ACCESS_TOKEN_SECRET is not defined in environment variables");
   }
-  
+
   return jwt.sign(
     { id: user.id, username: user.username, isAdmin: user.isAdmin },
     process.env.ACCESS_TOKEN_SECRET,
